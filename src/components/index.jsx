8d945586ea881b4f0493e9d685cac3139df54afd
OlawaleJoseph <adedekojoseph@gmail.com>
2020-01-10 17:58:39 +0100

    ft(refactor): change landing page navbar

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,10 +1,8 @@
 import React from 'react';
-import Nav from './navbar';
 import Landing from './landingPage';
 
 export default () => (
   <div>
-    <Nav />
     <Landing />
   </div>
 
--- src/components/landingPage/index.jsx
+++ src/components/landingPage/index.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
+import Navbar from '../navbar';
 
 export default () => (
-  <div className="container-fluid row justify-content-center align-items-center m-auto p-auto welcome-image">
-    <div className="d-flex flex-column text-center align-items-center">
+  <div className="container-fluid row justify-content-center align-items-center m-auto p-auto landing-page-image">
+    <Navbar />
+    <div className="overlay w-100 h-100" />
+    <div className="d-flex flex-column text-center align-items-center landing-message-div">
       <h3>Travel made easy</h3>
       <p>Travel easy. Travel fast. Travel smooth</p>
       <button className="btn btn-primary px-3" type="button">Travel Now!!!</button>
