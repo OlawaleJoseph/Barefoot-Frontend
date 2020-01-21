60f4a95aaa3eea476c669ac590198c8ee915931b
OlawaleJoseph <adedekojoseph@gmail.com>
2020-01-21 21:38:34 +0100

    bg(mobile-view): fix overlay height on mobile

--- src/components/landingPage/index.jsx
+++ src/components/landingPage/index.jsx
@@ -4,7 +4,7 @@ import Navbar from '../navbar';
 
 export default () => (
   <div className="container-fluid row justify-content-center align-items-center m-auto p-auto landing-page-image">
-    <div className="overlay w-100 h-100" />
+    <div className="overlay" />
     <Navbar />
     <div className="d-flex flex-column text-center align-items-center landing-message-div">
       <h3>Travel made easy</h3>
